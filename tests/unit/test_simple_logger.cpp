02636ba7aa0ce0d723eaa5f510a6b00f0bc7b6e7
Artem Ulyanov <aiden_pride@mail.ru>
2025-08-05 02:10:04 +0300

    Завершена система расширенных выходов (IOutput)
    - Реализованы ConsoleOutput и FileOutput
    - Добавлена поддержка множественных выходов в SimpleLogger
    - Обновлены unit-тесты для нового API
    - Обновлены примеры и документация
    
    Closes feature/ext-output

--- tests/unit/test_simple_logger.cpp
+++ tests/unit/test_simple_logger.cpp
@@ -1,22 +1,126 @@
 #include <gtest/gtest.h>
-
 #include <fstream>
-
+#include <sstream>
 #include "logger/simple_logger.hpp"
+#include "logger/console_output.hpp"
+#include "logger/file_output.hpp"
 
 using namespace stc;
 
-TEST(SimpleLoggerTest, ConsoleLog) {
+class CoutRedirect {
+public:
+    CoutRedirect(std::streambuf* new_buffer) : old(std::cout.rdbuf(new_buffer)) {}
+    ~CoutRedirect() { std::cout.rdbuf(old); }
+private:
+    std::streambuf* old;
+};
+
+TEST(SimpleLoggerTest, DefaultConstructorAddsConsoleOutput) {
     SimpleLogger logger;
-    EXPECT_NO_THROW(logger.log("Console test"));
+    
+    std::stringstream ss;
+    CoutRedirect redirect(ss.rdbuf());
+    
+    logger.log(LogLevel::Info, "Test message");
+    
+    std::string output = ss.str();
+    EXPECT_FALSE(output.empty());
+    EXPECT_NE(output.find("Test message"), std::string::npos);
 }
 
-TEST(SimpleLoggerTest, FileLog) {
-    const std::string fname = "test.log";
-    SimpleLogger logger(fname);
-    EXPECT_NO_THROW(logger.logToFile("File test"));
-    std::ifstream f(fname);
+TEST(SimpleLoggerTest, FileConstructorAddsFileOutput) {
+    const std::string filename = "test_file_only.log";
+    SimpleLogger logger(filename);
+    
+    std::stringstream ss;
+    CoutRedirect redirect(ss.rdbuf());
+    
+    logger.log(LogLevel::Info, "File message");
+    
+    EXPECT_TRUE(ss.str().empty());
+    
+    std::ifstream file(filename);
+    ASSERT_TRUE(file.is_open());
     std::string line;
-    std::getline(f, line);
-    EXPECT_NE(line.find("File test"), std::string::npos);
+    std::getline(file, line);
+    EXPECT_NE(line.find("File message"), std::string::npos);
+    file.close();
+
+    std::remove(filename.c_str());
+}
+
+TEST(SimpleLoggerTest, EmptyFilenameConstructorAddsConsoleOutput) {
+    SimpleLogger logger("");
+    
+    std::stringstream ss;
+    CoutRedirect redirect(ss.rdbuf());
+    
+    logger.log(LogLevel::Info, "Console message");
+    
+    std::string output = ss.str();
+    EXPECT_FALSE(output.empty());
+    EXPECT_NE(output.find("Console message"), std::string::npos);
+}
+
+TEST(SimpleLoggerTest, AddOutputWorks) {
+    SimpleLogger logger;
+    logger.clearOutputs();
+    
+    logger.addOutput(std::make_unique<ConsoleOutput>());
+    
+    std::stringstream ss;
+    CoutRedirect redirect(ss.rdbuf());
+    
+    logger.log(LogLevel::Warning, "Added output test");
+    
+    std::string output = ss.str();
+    EXPECT_NE(output.find("Added output test"), std::string::npos);
+}
+
+TEST(SimpleLoggerTest, ClearOutputsWorks) {
+    SimpleLogger logger;
+    
+    std::stringstream ss;
+    CoutRedirect redirect(ss.rdbuf());
+    
+    logger.log(LogLevel::Info, "Before clear");
+    EXPECT_FALSE(ss.str().empty());
+    
+    ss.str("");
+    ss.clear();
+    
+    logger.clearOutputs();
+    
+    logger.log(LogLevel::Info, "After clear");
+    EXPECT_TRUE(ss.str().empty());
+}
+
+TEST(SimpleLoggerTest, LogLevelFiltering) {
+    SimpleLogger logger;
+    logger.setLevel(LogLevel::Warning);
+    
+    std::stringstream ss;
+    CoutRedirect redirect(ss.rdbuf());
+
+    logger.log(LogLevel::Info, "Info message");
+    EXPECT_TRUE(ss.str().empty());
+    
+    logger.log(LogLevel::Warning, "Warning message");
+    EXPECT_NE(ss.str().find("Warning message"), std::string::npos);
+}
+
+TEST(SimpleLoggerTest, ILoggerInterfaceMethods) {
+    SimpleLogger logger;
+    
+    std::stringstream ss;
+    CoutRedirect redirect(ss.rdbuf());
+    
+    logger.info("Info test");
+    logger.warning("Warning test");
+    logger.error("Error test");
+    
+    std::string output = ss.str();
+    EXPECT_NE(output.find("Info test"), std::string::npos);
+    EXPECT_NE(output.find("Warning test"), std::string::npos);
+    EXPECT_NE(output.find("Error test"), std::string::npos);
 }
